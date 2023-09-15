ec285efe1e8e4971664496afaf5aec0d2360bf4b
effiecancode <faitheffie25@gmail.com>
2023-09-15 20:14:51 +0300

    books are meant to be organised

---
+++ 05-Book_keeper_easy/README.md
@@ -0,0 +1,7 @@
+For this project the application needs to have the following functionality:
+
+    Users should be able to add a book to their reading list by providing a book title, an author's name, and a year of publication.
+    The program should store information about all of these books in a file called books.csv, and this data should be stored in CSV format.
+    Users should be able to retrieve the books in their reading list, and these books should be printed out in a user-friendly format.
+    Users should be able to search for a specific book by providing a book title.
+    Users should be able to select these options from a text menu, and they should be able to perform multiple operations without restarting the program.
---
+++ 05-Book_keeper_easy/main.py
@@ -0,0 +1,68 @@
+# import csv
+
+def add_book():
+    Book_Title = input("Enter a tittle: ").strip().title()
+    Author = input("Enter the authors name: ").strip().title()
+    Year = int(input("Enter Year published: "))
+
+    with open("info.csv", mode="a") as book_lib:
+        book_lib.write(f"\n{Book_Title},{Author},{Year}")
+
+def get_books():
+    all_books = []
+
+    with open("info.csv", mode="r", encoding='UTF8') as book_lib:
+        book_contents = book_lib.readlines()
+        # print(f"{book_contents}")
+
+        if len(book_contents) < 2:
+            print("No books found!")
+            return
+
+        title = book_contents[0].strip().split(",")
+
+        for info in book_contents[1:]:
+            book_info = info.strip().split(",")
+            my_dict = dict(zip(title, book_info))
+            all_books.append(my_dict)
+
+        for book_info in all_books:
+            print(book_info)
+
+    # for i, line in enumerate(lib_contents, start=1):
+    #     print(f"{i}: {line.strip()}")
+
+def search_book():
+    with open("info.csv", mode="r", encoding='UTF8') as books:
+        lib_contents = books.read()
+        print(f"{lib_contents}")
+
+    search_title = input("Enter book tittle: ")
+    for book in search_title:
+        if search_title in book:
+            print(book)
+
+        else:
+            print("INVALID BOOK NAME OR INCORRECT SPELLING")
+
+def exit():
+    pass
+
+option = int(input(f"\nSelect an action\n \n(1) Add a Book \n(2) Get all Books \n(3) Search a book \n(4) Exit Program \n\nEnter Option: "))
+print()
+
+# while True:
+if option == 1:
+    add_book()
+
+elif option == 2:
+    get_books()
+
+elif option == 3:
+    search_book()
+
+elif option == 4:
+    exit()
+
+else:
+    print("INVALID OPTION")
